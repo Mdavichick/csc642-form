79c55120b9ee3d40ceec306f76da26feeaa43f3e
Mdavichick <78510482+Mdavichick@users.noreply.github.com>
2021-11-30 13:11:33 -0800

    Google Maps Working. Pushing

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,7 +1,15 @@
 import React,{Component} from 'react';
+import { Loader } from "@googlemaps/js-api-loader"
+// import {GoogleMap} from "react-google-maps";
+import { Google } from './GoogleMaps';
+
+
+
+
 
 
 export class Confirmation extends React.Component {
+  
   state ={
     lname: localStorage.getItem('lname'),
     fname:localStorage.getItem('fname'),
@@ -14,20 +22,32 @@ export class Confirmation extends React.Component {
     addzip:localStorage.getItem('addzip'),
     email:localStorage.getItem('email'),
   }
+
   render() {
+   
     return (
       <div className ="container">
       <div className="app-wrapper">
       <h2 className="title">Results Page</h2>
 
-      
-        <h1>{this.state.lname}</h1>
-        <h1>{this.state.pnumber}</h1>
-        <h1> TEST </h1>
      
+        <h3 className="showaddress"><small>Welcome</small> {this.state.fname} {this.state.lname}</h3>
+        <br/>
+       
+        <h3 className="showaddress"><small>Preferred title:</small> {this.state.title}</h3>
+        <h3 className="showaddress"><small>Height:</small> {this.state.heightfeet} feet, {this.state.heightinch} inches </h3>
+        <h3 className="showaddress"><small>Phone Number:</small> {this.state.pnumber}</h3>
+        <h3 className="showaddress"><small>Address:</small> {this.state.addcity}, {this.state.addstate}, {this.state.addzip}</h3>
+        <h3 className="showaddress"><small>Email:</small> {this.state.email}</h3>
+        
+        <Google/>
+       </div>
+       </div>
+        
+        
+
+       
      
-        </div>
-      </div>
 
     )
   }
