9d6eda856ef44bc7a927af74f397900058805ccf
Mdavichick <78510482+Mdavichick@users.noreply.github.com>
2021-11-30 14:25:54 -0800

    github pages attempt

---
+++ Docs/src/components/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SurveyForm from "./SurveyForm";
+
+const Form = () => {
+    return (
+        <div>        
+            <SurveyForm />
+        </div>
+    )
+}
+
+export default Form
---
+++ Docs/src/components/GoogleMaps.js
@@ -0,0 +1,72 @@
+import React,{Component} from 'react';
+import { GoogleMap, useLoadScript, Marker, InfoWindow, useJsApiLoader } from '@react-google-maps/api';
+import MapStyles from '../MapStyles';
+
+import usePlacesAutocomplete, {getGeocode, getLatLng,} from "use-places-autocomplete";
+
+
+const libraries = ["places"];
+const mapContainerStyle ={
+    width: '300px',
+    height: '300px',
+}
+const center ={
+    lat:37.774929,
+    lng: -122.419418,
+}
+const options ={
+    styles: MapStyles,
+    disableDefaultUI: true,
+    // zoomControl: true,
+}
+const state ={
+    lname: localStorage.getItem('lname'),
+    fname:localStorage.getItem('fname'),
+    title:localStorage.getItem('title'),
+    heightinch:localStorage.getItem('heightinch'),
+    heightfeet:localStorage.getItem('heightfeet'),
+    pnumber:localStorage.getItem('pnumber'),
+    addcity:localStorage.getItem('addcity'),
+    addstate:localStorage.getItem('addstate'),
+    addzip:localStorage.getItem('addzip'),
+    email:localStorage.getItem('email'),
+  }
+
+
+  
+export function Google(){
+    const mapRef = React.useRef();
+    const onMapLoad= React.useCallback((map) =>{
+        mapRef.current = map;
+    }, [])
+
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: 'AIzaSyCKpHxuTD87SuiNqWf6Q-B5PIeWOuKuXT0',
+        libraries,
+    })
+
+    if(loadError) return "Error loading maps";
+    if(!isLoaded) return "Loading Maps";
+    
+
+    // <div className ="container">
+    // <div className="app-wrapper">
+    return<div className ="app-wrapper">
+        
+        
+        <div className="showaddress">
+        {localStorage.getItem('addcity')},{localStorage.getItem('addstate')},{localStorage.getItem('addzip')}
+        <GoogleMap 
+        className = "Test"
+        mapContainerStyle={mapContainerStyle} 
+        zoom={13} 
+        center = {center}
+        options = {options}
+        onLoad={onMapLoad}
+        onClick={(event)=>{
+            console.log(event);
+        }}
+        ></GoogleMap>
+        </div></div>
+        
+}
